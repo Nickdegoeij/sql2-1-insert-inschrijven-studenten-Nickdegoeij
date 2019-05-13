f6b7b69c02280334a835b1782e3b544384dc6141
Nickdegoeij <nickdegoeij54@gmail.com>
2019-05-13 10:06:29 +0200

    Update inschrijvingen.sql

---
+++ inschrijvingen.sql
@@ -0,0 +1,4 @@
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES 
+    ('lo9e-amo1','akr02', 2019, 'amo'), 
+    ('lo9e-amo2','brc01', 2019, 'amo'), 
+    ('lo9e-amo3','bkr02', 2019, 'amo');
